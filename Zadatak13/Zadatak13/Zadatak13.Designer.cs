a9af51a4d71d34405de31123115bbdbbfee9498d
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-10-09 08:34:16 +0200

    Add files via upload

---
+++ Zadatak13/Zadatak13/Zadatak13.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Zadatak13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOtvori = new System.Windows.Forms.Button();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOtvori
+            // 
+            this.btnOtvori.Location = new System.Drawing.Point(264, 76);
+            this.btnOtvori.Name = "btnOtvori";
+            this.btnOtvori.Size = new System.Drawing.Size(222, 47);
+            this.btnOtvori.TabIndex = 0;
+            this.btnOtvori.Text = "Otvorite Message Box";
+            this.btnOtvori.UseVisualStyleBackColor = true;
+            this.btnOtvori.Click += new System.EventHandler(this.btnOtvori_Click);
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(134, 152);
+            this.txtIspis.Multiline = true;
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(491, 269);
+            this.txtIspis.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.btnOtvori);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOtvori;
+        private System.Windows.Forms.TextBox txtIspis;
+    }
+}
+
---
+++ Zadatak13/Zadatak13/Zadatak13.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string poruka, naslov, buttons, ikona;
+
+        private void btnOtvori_Click(object sender, EventArgs e)
+        {
+
+            string poruka = "Odaberite Da / Ne / Odustani";
+            string naslov = "Naslovna linija Dijaloga za poruke";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            switch (rez) { 
+
+                case DialogResult.Yes:
+
+                txtIspis.Text = "Izabrali ste dugme DA";
+
+                break;
+
+            case DialogResult.No:
+
+                txtIspis.Text = "Izabrali ste dugme NE";
+
+                break;
+
+            case DialogResult.Cancel:
+
+                txtIspis.Text = "Izabrali ste dugme ODUSTANI";
+
+                break;
+            }
+        }
+    }
+}
